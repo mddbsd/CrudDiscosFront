bd269d3787a149dcb228019e3ad6e25f6ac28106
max <mdd.bsd@gmail.com>
2024-09-09 11:40:06 -0300

    implemetadas las RUTAS con React Router DOM

---
+++ src/componentes/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Landing from '../assets/landing.png'
+
+export default function Home() {
+  return (
+    <>
+    <section className="bg-top bg-fixed bg-no-repeat bg-gray-700 bg-blend-multiply" style={{backgroundImage: `url(${Landing})`}}>
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Bienvenidos</h1>
+            <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Esta es la pagina de bienvenida</p>
+            <a  href="./discos" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Ver Discos
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+            <a  href="./info" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                info
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+            <a  href="./proximamente" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                proximanente
+                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    </section>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,43 @@
+//Modulos
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//Componentes
 import App from './App.jsx'
 import Header from './componentes/Header.jsx'
 import Footer from './componentes/Footer.jsx'
+import Home from './componentes/Home.jsx'
+import Proximamente from './componentes/Proximamente.jsx'
+import Info from './componentes/Info.jsx'
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: (<div>Error esto esta mal</div>),
+  },
+  {
+    path: "/discos",
+    element: <App />,
+    errorElement: (<div>disco no esta</div>),
+  },
+  {
+    path: "/info",
+    element: <Info />,
+    errorElement: (<div>disco no esta</div>),
+  },
+  {
+    path: "/proximamente",
+    element: <Proximamente />,
+    errorElement: (<div>disco no esta</div>),
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <App />
+    <RouterProvider router={router} />
     <Footer />
   </StrictMode>,
 )
